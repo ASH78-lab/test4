791717babb9a7c60297e776d4c9b8cefe4842fe9
ASH78-lab <roben4300@gmail.com>
2026-06-10 21:03:34 +0300

    Update par_adv.py

--- par_adv.py
+++ par_adv.py
@@ -50,7 +50,7 @@ def pin_all():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     
